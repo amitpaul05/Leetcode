66f8e03963844e9cc172931536bcd8465ae1c44e
Amit Paul <131515970+amitpaul05@users.noreply.github.com>
2025-04-04 13:20:39 +0600

    Time: 6 ms (77.62%) | Memory: 18 MB (96.83%) - LeetSync

--- leetcode_75/605-can-place-flowers/can-place-flowers.py
+++ leetcode_75/605-can-place-flowers/can-place-flowers.py
@@ -3,7 +3,9 @@ class Solution:
         f = [0] + flowerbed + [0]
         i = 1
         while i < len(f)-1:
-            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
+            if f[i] == 1:
+                i += 2
+            elif f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
                 f[i] = 1
                 n -= 1
                 i += 2
