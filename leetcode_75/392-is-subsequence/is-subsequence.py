fcf1440235c5382d65399988a6c1d15418d648fa
Amit Paul <131515970+amitpaul05@users.noreply.github.com>
2025-04-05 12:32:09 +0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.25%) - LeetSync

--- leetcode_75/392-is-subsequence/is-subsequence.py
+++ leetcode_75/392-is-subsequence/is-subsequence.py
@@ -1,10 +1,8 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         j = 0
-        s_len = 0 # number of matched char
         for i in range(len(t)):
             if j < len(s) and s[j] == t[i]:
-                s_len += 1
                 j += 1
         
-        return s_len == len(s)
+        return j == len(s)
