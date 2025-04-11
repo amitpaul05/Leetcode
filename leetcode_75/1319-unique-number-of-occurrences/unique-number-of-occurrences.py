7ffeaa0e9a31aa027dbb6b15c59c49239aaba994
Amit Paul <131515970+amitpaul05@users.noreply.github.com>
2025-04-11 11:50:36 +0600

    Time: 1 ms (25.42%) | Memory: 18 MB (40.73%) - LeetSync

--- leetcode_75/1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ leetcode_75/1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,7 +1,11 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        set_arr = set(arr)
-        occurrence = []
-        for i in set_arr:
-            occurrence.append(arr.count(i))
-        return len(occurrence) == len(set(occurrence))
+        # set_arr = set(arr)
+        # occurrence = []
+        # for i in set_arr:
+        #     occurrence.append(arr.count(i))
+        # return len(occurrence) == len(set(occurrence))
+
+        hMap = Counter(arr)
+
+        return len(set(arr)) == len(set(hMap.values()))
