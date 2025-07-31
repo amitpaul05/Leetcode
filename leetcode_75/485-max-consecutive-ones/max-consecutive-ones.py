816cce286062d2910a22c2994753752f34bcaf3d
Amit Paul <131515970+amitpaul05@users.noreply.github.com>
2025-07-31 23:33:02 +0600

    Time: 8 ms (92.28%) | Memory: 20.2 MB (55.70%) - LeetSync

--- leetcode_75/485-max-consecutive-ones/max-consecutive-ones.py
+++ leetcode_75/485-max-consecutive-ones/max-consecutive-ones.py
@@ -1,22 +1,35 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        left = 0
-        right = left + 1
-        count = 0
-        if nums.count(1) == 0:
-            return 0
-        while left < len(nums) and right < len(nums):
-            if nums[left] != 1:
-                left += 1
-                right = left + 1
-            elif nums[right] != 1:
-                count = max(count,right - left)
-                left = right + 1
-                right = left + 1
-            elif nums[left] == 1 and nums[right] == 1:
-                right += 1
-        count = count = max(count,right - left)
-        return count
+        # left = 0
+        # right = left + 1
+        # count = 0
+        # if nums.count(1) == 0:
+        #     return 0
+        # while left < len(nums) and right < len(nums):
+        #     if nums[left] != 1:
+        #         left += 1
+        #         right = left + 1
+        #     elif nums[right] != 1:
+        #         count = max(count,right - left)
+        #         left = right + 1
+        #         right = left + 1
+        #     elif nums[left] == 1 and nums[right] == 1:
+        #         right += 1
+        # count = count = max(count,right - left)
+        # return count
+
+        current_max=0
+        consecutive_sum=0 
+        for i in nums:
+            if i==1:
+                consecutive_sum+=1
+            else:
+                if consecutive_sum>current_max:
+                    current_max=consecutive_sum
+                consecutive_sum=0
+        if consecutive_sum>current_max:
+            current_max=consecutive_sum
+        return current_max
 
 
         
