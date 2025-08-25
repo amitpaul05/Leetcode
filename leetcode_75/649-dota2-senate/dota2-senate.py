6bfb21f0780ce089c21de258221dfe22eeccb768
Amit Paul <131515970+amitpaul05@users.noreply.github.com>
2025-08-25 08:01:56 +0600

    Time: 711 ms (7.08%) | Memory: 18.4 MB (19.10%) - LeetSync

--- leetcode_75/649-dota2-senate/dota2-senate.py
+++ leetcode_75/649-dota2-senate/dota2-senate.py
@@ -1,9 +1,14 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         n = len(senate)
+        r_q = []
+        d_q = []
 
-        r_q = [i for i in range(len(senate)) if senate[i]=='R']
-        d_q = [j for j in range(len(senate)) if senate[j]=='D']
+        for i in range(len(senate)):
+            if senate[i] == 'R':
+                r_q.append(i)
+            else:
+                d_q.append(i)
             # if r_q and d_q:
             #     if d_q[0] > r_q[0]:
             #         r_q.append(n)
@@ -22,24 +27,4 @@ class Solution:
             r_q = r_q[1:]
             d_q = d_q[1:]
         return 'Radiant' if r_q else 'Dire'
-
-
-
-# class Solution:
-#     def predictPartyVictory(self, senate: str) -> str:
-
-#         n = len(senate)
-
-#         r_arr = [i for i in range(len(senate)) if senate[i]=='R']
-#         d_arr = [j for j in range(len(senate)) if senate[j]=='D']
-        
-#         while r_arr and d_arr:
-#             r = r_arr.pop(0)
-#             d = d_arr.pop(0)
-#             if r < d:
-#                 r_arr.append(n + r)
-#             else:
-#                 d_arr.append(n + d)
-                
-#         return 'Radiant' if r_arr else 'Dire'
         
