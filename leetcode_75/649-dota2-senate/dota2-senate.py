31be9a1cb2864c2ff26cf29d88b3bf93fba2d0b9
Amit Paul <131515970+amitpaul05@users.noreply.github.com>
2025-08-25 07:50:55 +0600

    Time: 67 ms (15.60%) | Memory: 18.3 MB (34.05%) - LeetSync

--- leetcode_75/649-dota2-senate/dota2-senate.py
+++ leetcode_75/649-dota2-senate/dota2-senate.py
@@ -1,30 +1,50 @@
+# class Solution:
+#     def predictPartyVictory(self, senate: str) -> str:
+#         n = len(senate)
+#         r_q = []
+#         d_q = []
+
+#         for i in range(len(senate)):
+#             if senate[i] == 'R':
+#                 r_q.append(i)
+#             else:
+#                 d_q.append(i)
+#             # if r_q and d_q:
+#             #     if d_q[0] > r_q[0]:
+#             #         r_q.append(n)
+#             #         n += 1
+#             #     elif d_q[0] < r_q[0]:
+#             #         d_q.append(n)
+#             #     r_q = r_q[1:]
+#             #     d_q = d_q[1:]
+#         while r_q and d_q:
+#             if d_q[0] > r_q[0]:
+#                 r_q.append(n)
+#                 n += 1
+#             elif d_q[0] < r_q[0]:
+#                 d_q.append(n)
+#                 n += 1
+#             r_q = r_q[1:]
+#             d_q = d_q[1:]
+#         return 'Radiant' if r_q else 'Dire'
+
+
+
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        n = len(senate)
-        r_q = []
-        d_q = []
 
-        for i in range(len(senate)):
-            if senate[i] == 'R':
-                r_q.append(i)
+        n = len(senate)
+
+        r_arr = [i for i in range(len(senate)) if senate[i]=='R']
+        d_arr = [j for j in range(len(senate)) if senate[j]=='D']
+        
+        while r_arr and d_arr:
+            r = r_arr.pop(0)
+            d = d_arr.pop(0)
+            if r < d:
+                r_arr.append(n + r)
             else:
-                d_q.append(i)
-            # if r_q and d_q:
-            #     if d_q[0] > r_q[0]:
-            #         r_q.append(n)
-            #         n += 1
-            #     elif d_q[0] < r_q[0]:
-            #         d_q.append(n)
-            #     r_q = r_q[1:]
-            #     d_q = d_q[1:]
-        while r_q and d_q:
-            if d_q[0] > r_q[0]:
-                r_q.append(n)
-                n += 1
-            elif d_q[0] < r_q[0]:
-                d_q.append(n)
-                n += 1
-            r_q = r_q[1:]
-            d_q = d_q[1:]
-        return 'Radiant' if r_q else 'Dire'
+                d_arr.append(n + d)
+                
+        return 'Radiant' if r_arr else 'Dire'
         
