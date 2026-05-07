22f838e984cbc2b0e1953ba7d0a8719faadea336
Amit Paul <131515970+amitpaul05@users.noreply.github.com>
2026-05-07 21:16:45 +0600

    Time: 4 ms (2.69%) | Memory: 19.4 MB (54.59%) - LeetSync

---
+++ leetcode_75/101-symmetric-tree/symmetric-tree.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leftTreeEqualRightTree(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if root1.left and root2.right:
+            if root1.left.val != root2.right.val:
+                return False
+            return self.leftTreeEqualRightTree(root1.left, root2.right)
+        elif not root1.left and not root2.right:
+            return True
+        return False
+    
+    def rightTreeEqualLeftTree(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        print(root1.right, root2.left)
+        if root1.right and root2.left:
+            if root1.right.val != root2.left.val:
+                return False
+            return self.rightTreeEqualLeftTree(root1.right, root2.left)
+        elif not root1.right and not root2.left:
+            return True
+        return False
+        
+
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def is_mirror(n1, n2):
+            if not n1 and not n2:
+                return True
+            
+            if not n1 or not n2:
+                return False
+            
+            return n1.val == n2.val and is_mirror(n1.left, n2.right) and is_mirror(n1.right, n2.left)
+        
+        return is_mirror(root.left, root.right)
+        
