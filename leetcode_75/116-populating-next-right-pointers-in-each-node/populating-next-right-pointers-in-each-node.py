712e399e6aba4197c64752264d81648b1101a666
Amit Paul <131515970+amitpaul05@users.noreply.github.com>
2026-06-28 18:14:55 +0600

    Time: 109 ms (5.07%) | Memory: 20.7 MB (17.42%) - LeetSync

--- leetcode_75/116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ leetcode_75/116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,26 +1,45 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
+# """
+# # Definition for a Node.
+# class Node:
+#     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#         self.next = next
+# """
+
+# class Solution:
+#     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+#         if root:
+#             q = deque([root])
+#             while q:
+#                 l_size = len(q)
+#                 for i in range(l_size):
+#                     current = q.popleft()
+#                     if i < l_size-1:
+#                         current.next = q[0]
+#                     if current.left:
+#                         q.append(current.left)
+#                     if current.right:
+#                         q.append(current.right)
+#                 current.next = None
+#         return root
+
 
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if root:
-            q = deque([root])
-            while q:
-                l_size = len(q)
-                for i in range(l_size):
-                    current = q.popleft()
-                    if i < l_size-1:
-                        current.next = q[0]
-                    if current.left:
-                        q.append(current.left)
-                    if current.right:
-                        q.append(current.right)
-                current.next = None
-        return root
+    
+    def compute(self,left,right):
+        if not left or not right:
+            return
+        left.next = right
+        self.compute(left.right,right.left)
+        self.compute(left.left,left.right)
+        self.compute(right.left,right.right)
+        
+        
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return 
+        self.compute(root.left,root.right)
+        return(root)
